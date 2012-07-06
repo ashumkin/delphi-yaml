076d47e00bf7901149e86a7ac2d2f356ed4164cc
Ivan_Levashev <devnull@localhost>
2012-07-06 19:33:34 +0700

    Тонкая привязка собирается

---
+++ compile.cmd
@@ -0,0 +1,8 @@
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -oapi.obj api.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -oreader.obj reader.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -oscanner.obj scanner.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -oparser.obj parser.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -oloader.obj loader.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -owriter.obj writer.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -oemitter.obj emitter.c
+bcc32 -DHAVE_CONFIG_H -I. -I.. -I..\include -v -c -odumper.obj dumper.c
