b5c082b213ad26b6879bef924318cbd0dc5c86eb
Ivan Levashew <octagram@bluebottle.com>
2012-09-03 16:41:42 +0700

    fix tests; one more test is working for parsing sexagesimal ints

--- YamlTestsThick.pas
+++ YamlTestsThick.pas
@@ -12,6 +12,7 @@ type
   TYamlTestsThick = class(TTestCase)
   published
     procedure TestParse;
+    procedure TestParseAdvanced;
   end;
 
 { TYamlTestsThick }
@@ -19,7 +20,7 @@ type
 procedure TYamlTestsThick.TestParse;
 begin
   CheckTrue(
-    FromYaml(
+    LoadYaml(
       'testdict:'#13#10 +
       '  - 2'#13#10 +
       '  -'#13#10 +
@@ -39,5 +40,39 @@ begin
   );
 end;
 
+procedure TYamlTestsThick.TestParseAdvanced;
+begin
+  CheckTrue(
+    LoadYaml(
+      'testdict:'#13#10 +
+      '  - 2'#13#10 +
+      '  -'#13#10 +
+      '  - ["4", true]'#13#10 +
+      '"another key": abc'#13#10 +
+      'something different:'#13#10 +
+      '  - TRUE'#13#10 +
+      '  - 20:2:02'#13#10 +
+      '  - ''20:2:02'''#13#10 +
+      '  - "20:2:02"'#13#10
+    ).Equals(CMap([
+      'testdict', VList([
+        2,
+        nil,
+        VList(['4', True])
+      ]),
+      'another key', 'abc',
+      'something different', VList([
+        True,
+        (20 * 60 + 2) * 60 + 2,
+        '20:2:02',
+        '20:2:02'
+      ])
+    ]))
+  );
+end;
+
 initialization
+  RegisterTest(
+    TTestSuite.Create('Yaml thick tests',
+    [TYamlTestsThick.Suite]));
 end.
